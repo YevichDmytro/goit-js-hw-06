8ecbf08897d7e213693bd79186f0973e0065dfba
Dmytro <57775385+YevichDmytro@users.noreply.github.com>
2024-03-28 22:01:30 +0200

    finish

---
+++ js/task-3.js
@@ -0,0 +1,43 @@
+class StringBuilder {
+  #value;
+
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+
+  getValue() {
+    return this.#value;
+  }
+
+  padStart(str) {
+    this.#value = str.concat(this.#value);
+  }
+
+  padEnd(str) {
+    this.#value += str;
+  }
+
+  //Option 1
+  padBoth(str) {
+    this.padStart(str);
+    this.padEnd(str);
+  }
+
+  //Option 2
+  //   padBoth(str) {
+  //     this.#value = str.concat(this.#value) + str;
+  //   }
+}
+
+const builder = new StringBuilder('.');
+
+console.log(builder.getValue()); // "."
+
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
